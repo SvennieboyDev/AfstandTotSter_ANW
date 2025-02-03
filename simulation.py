9dd9f6bebecc8e5bcbca74464f5d982a094db96b
SvennieboyDev <Sven.Deijkers@gmail.com>
2025-02-03 23:16:57 +0100

    Beweging aarde rond de zon
    3-2-2025
    
    De aarde beweegt nu rond de zon in de baan die er is getekend. Dit heb ik gedaan door de formules toe te passen om een ellips te tekenen.
    
    De baan van de aarde is nu wel een perfecte cirkel, daar moeten de kleine afwijkingen nog bij komen. Ook gaat de aarde nog veel te snel.

--- simulation.py
+++ simulation.py
@@ -1,12 +1,28 @@
-import pygame    
+import pygame
+import math
+
+# Aarde beweging
+center_x, center_y = 500, 500
+a, b = 400, 400
+t_earth = 0
 
 def heliocentricSystem(screen):
     # Zon
     pygame.draw.circle(screen, (255, 245, 237), (500, 500), 100, 100) # Kleur aangeraden door ChatGPT, lijkt wit vanaf de ruimte gezien
 
     # Aarde
-    pygame.draw.ellipse(screen, (255, 255, 255), (100, 200, 800, 600), width=1)
-    pygame.draw.circle(screen, (28, 163, 236), (500, 200), 10, 10) # Kleur van ondiep water, turquoise of cyaan, door ChatGPT
+    pygame.draw.ellipse(screen, (255, 255, 255), (100, 100, 800, 800), width=1)
+    
+    x, y = earthOrbit()
+    pygame.draw.circle(screen, (28, 163, 236), (x, y), 10, 10) # Kleur van ondiep water, turquoise of cyaan, door ChatGPT
+
+def earthOrbit():
+    global t_earth
+    x = center_x + a * math.cos(t_earth)
+    y = center_y + b * math.sin(t_earth)
+
+    t_earth += 0.01
+    return x, y
 
 def main():
     pygame.init()
