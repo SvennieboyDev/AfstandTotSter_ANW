2d1979b1e1c98e4ac27a686863c46ff60a61c8fd
SvennieboyDev <Sven.Deijkers@gmail.com>
2025-02-07 11:47:09 +0100

    Algemene Baan calculator, maan en alles variabelen
    7-2-2025
    
    de earthOrbit() function is nu niet specifiek voor de aarde, maar voor alle hemellichamen. Dat komt door de vele variabelen die nu zijn gebruikt.
    
    Verder is er nu een maan aanwezig, die gebruikt dan dus ook dezelfde earthOrbit() function, omdat het dezelfde variabelen, die daarvoor zijn gespecificeerd, geeft, maar dan met andere waardes.

--- simulation.py
+++ simulation.py
@@ -1,32 +1,51 @@
 import pygame
 import math
 
-# Aarde beweging
-center_x, center_y = 500, 500
-a, b = 400, 400
 t_earth = 0
+t_moon = 0
 
 def heliocentricSystem(screen):
+    center_x, center_y = 350, 350 #Makkelijkere manier?
+
     # Zon
-    pygame.draw.circle(screen, (255, 245, 237), (500, 500), 100, 100) # Kleur aangeraden door ChatGPT, lijkt wit vanaf de ruimte gezien
+    sun_size = 100
+    pygame.draw.circle(screen, (255, 245, 237), (center_x, center_y), sun_size, sun_size) # Kleur aangeraden door ChatGPT, lijkt wit vanaf de ruimte gezien
 
     # Aarde
-    pygame.draw.ellipse(screen, (255, 255, 255), (100, 100, 800, 800), width=1)
-    
-    x, y = earthOrbit()
-    pygame.draw.circle(screen, (28, 163, 236), (x, y), 10, 10) # Kleur van ondiep water, turquoise of cyaan, door ChatGPT
-
-def earthOrbit():
+    earth_a, earth_b = 250, 250
     global t_earth
-    x = center_x + a * math.cos(t_earth)
-    y = center_y + b * math.sin(t_earth)
 
-    t_earth += 0.01
-    return x, y
+    border_distance = 100
+    earth_orbit_size = 500
+    pygame.draw.ellipse(screen, (255, 255, 255), (border_distance, border_distance, earth_orbit_size, earth_orbit_size), width=1)
+    
+    t_earth_increment = 0.01
+    earth_x, earth_y, t_earth = createOrbit(center_x, center_y, earth_a, earth_b, t_earth, t_earth_increment)
+    pygame.draw.circle(screen, (28, 163, 236), (earth_x, earth_y), 15, 15) # Kleur van ondiep water, turquoise of cyaan, door ChatGPT
+
+    # Maan
+    moon_a, moon_b = 30, 30
+    global t_moon
+
+    #moon_orbit_size = 60
+    #pygame.draw.ellipse(screen, (255, 255, 255), (earth_x - moon_a, earth_y - moon_b, moon_orbit_size, moon_orbit_size), width=1)
+
+    t_moon_increment = t_earth_increment * 13.4
+    moon_x, moon_y, t_moon = createOrbit(earth_x, earth_y, moon_a, moon_b, t_moon, t_moon_increment)
+    pygame.draw.circle(screen, (255, 255, 255), (moon_x, moon_y), 3, 3)
+
+
+def createOrbit(center_x, center_y, a, b, t, t_increment):    
+    x = center_x + a * math.cos(t)
+    y = center_y + b * math.sin(t)
+
+    t += t_increment
+    return x, y, t
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1000, 1000))
+    screen = pygame.display.set_mode((700, 700))
     pygame.display.set_caption("Afstand tot ster")
     clock = pygame.time.Clock()
     running = True
